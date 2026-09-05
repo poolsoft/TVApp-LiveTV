841d8875350b2e91f24a62d745f99561345a2d27
mgulacti <muhammedgulacti@kahramanmaras.bel.tr>
2026-09-05 22:37:07 +0300

    fix: add missing ImageView import in MainActivity

--- app/src/main/java/com/tvapp/livetv/MainActivity.kt
+++ app/src/main/java/com/tvapp/livetv/MainActivity.kt
@@ -20,6 +20,7 @@ import android.view.View
 import android.view.ViewConfiguration
 import android.view.WindowManager
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.ArrayAdapter
 import android.widget.AdapterView
 import android.widget.LinearLayout
